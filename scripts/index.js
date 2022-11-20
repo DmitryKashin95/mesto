a874cf9f0641f4afd7f50924e11bda523a48614a
Kashin Dmitry <dmitrykashin95@yandex.ru>
2022-11-20 13:37:37 +0300

    hotfixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__button-close');
-const saveButton = popup.querySelector('.popup__button-save');
+const saveButton = popup.querySelector('.popup__form');
 
 const popupContainer = popup.querySelector('.popup__container');
-const popupName = popupContainer.querySelector('#name');
-const popupProf = popupContainer.querySelector('#prof');
+const popupName = popupContainer.querySelector('.popup__input_js_name');
+const popupProf = popupContainer.querySelector('.popup__input_js_prof');
 
 const profileInfo = document.querySelector('.profile__info');
 const editButton = profileInfo.querySelector('.profile__button-edit');
@@ -12,23 +12,19 @@ const addButton = profileInfo.querySelector('.profile__button-add');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 
-function addClass() {
-  popup.classList.add('popup__opened');
-  inputInfo()
-};
-
-function removeClass() {
-  popup.classList.remove('popup__opened');
-};
-
-editButton.addEventListener('click', addClass);
-closeButton.addEventListener('click', removeClass);
-saveButton.addEventListener('click', changeInfo);
-
 function inputInfo() {
   popupName.value = profileName.textContent;
   popupProf.value = profileProfession.textContent;
 }
+
+function addClass() {
+  popup.classList.add('popup_opened');
+  inputInfo()
+};
+
+function removeClass() {
+  popup.classList.remove('popup_opened');
+};
 
 function changeInfo(evt) {
   evt.preventDefault();
@@ -36,3 +32,7 @@ function changeInfo(evt) {
   profileProfession.textContent = popupProf.value;
   removeClass();
 }
+
+editButton.addEventListener('click', addClass);
+closeButton.addEventListener('click', removeClass);
+saveButton.addEventListener('submit', changeInfo);
