3a2828891ed8722a41f6372f8f245022b8d8f747
Kashin Dmitry <dmitrykashin95@yandex.ru>
2022-12-15 21:21:16 +0300

    fix-names

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 const popups = document.querySelector('.popup');
 const cardTemplate = document.querySelector('#newCard').content;
-const closeButton = document.querySelectorAll('.popup__button-close');
+const closeButtons = document.querySelectorAll('.popup__button-close');
 const saveButton = popups.querySelector('#popup__form_edit');
 const saveCardButton = popups.querySelector('#popup__form_add');
 const popupImgOpen = document.querySelector('.popup_img-open')
 
 const popupEditProfile = document.querySelector('.popup_edit-profile');
 const formEditProfile = popupEditProfile.querySelector('.popup__form');
-const popupName = popupEditProfile.querySelector('.popup__input_js_name');
-const popupProf = popupEditProfile.querySelector('.popup__input_js_prof');
+const inputUserName = popupEditProfile.querySelector('.popup__input_js_name');
+const inputUserProfession = popupEditProfile.querySelector('.popup__input_js_prof');
 const popupAddCard = document.querySelector('.popup_add-card');
 const formAddCard = popupAddCard.querySelector('.popup__form');
-const addCardName = popupAddCard.querySelector('.popup__input_card-name');
-const addCardLink = popupAddCard.querySelector('.popup__input_card-link');
+const inputCardTitle = popupAddCard.querySelector('.popup__input_card-name');
+const inputCardLink = popupAddCard.querySelector('.popup__input_card-link');
 
 const profileInfo = document.querySelector('.profile__info');
-const editButton = profileInfo.querySelector('.profile__button-edit');
+const buttonOpenEditProfilePopup = profileInfo.querySelector('.profile__button-edit');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const addButton = document.querySelector('.profile__button-add');
+const buttonOpenAddCardPopup = document.querySelector('.profile__button-add');
 const cardsElement = document.querySelector('.cards')
 const likeActive = document.querySelectorAll('.card__like');
 const popupImg = document.querySelector('.popup__img');
 const popupImgTitle = document.querySelector('.popup__img-title');
 
 function inputInfo() {
-  popupName.value = profileName.textContent;
-  popupProf.value = profileProfession.textContent;
+  inputUserName.value = profileName.textContent;
+  inputUserProfession.value = profileProfession.textContent;
 };
 
 function openPopup(popup) {
@@ -47,12 +47,12 @@ function deleteCard (evt) {
 
 function changeInfo(evt) {
   evt.preventDefault();
-  profileName.textContent = popupName.value;
-  profileProfession.textContent = popupProf.value;
+  profileName.textContent = inputUserName.value;
+  profileProfession.textContent = inputUserProfession.value;
   closePopup(popupEditProfile);
 };
 
-function increasePopup (link, name) {
+function openImagePopup (link, name) {
   popupImg.src = link;
   popupImg.alt = name;
   popupImgTitle.textContent = name;
@@ -69,7 +69,7 @@ function createCard(link, name) {
 
   cardElement.querySelector('.card__like').addEventListener('click', toogleLike);
   cardElement.querySelector('.card__delete').addEventListener('click', deleteCard);
-  cardImage.addEventListener('click', () => increasePopup(link, name));
+  cardImage.addEventListener('click', () => openImagePopup(link, name));
   return cardElement;
 };
 
@@ -81,22 +81,22 @@ function addCards (cards) {
 
 function addCard (evt) {
   evt.preventDefault();
-  cardsElement.prepend(createCard(addCardLink.value, addCardName.value));
+  cardsElement.prepend(createCard(inputCardLink.value, inputCardTitle.value));
   formAddCard.reset();
   closePopup(popupAddCard);
 };
 
 addCards(initialCards);
 
-editButton.addEventListener('click', function () {
+buttonOpenEditProfilePopup.addEventListener('click', function () {
   openPopup(popupEditProfile);
   inputInfo()
 });
-addButton.addEventListener('click', function () {
+buttonOpenAddCardPopup.addEventListener('click', function () {
   openPopup(popupAddCard);
 });
 
-closeButton.forEach(function(elem) {
+closeButtons.forEach(function(elem) {
   const closePopups = elem.closest('.popup');
   elem.addEventListener('click', function() {
     closePopup(closePopups);
